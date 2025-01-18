b0c1c8026db46d9e11bc993cd9c13dcbba5c1b56
done <chalghoummaissa@gmail.com>
2025-01-18 21:21:25 +0100

    formulaire borrow(backend pas complet) et interface hist_borrow

--- app/src/main/java/com/example/libraryland/BorrowBookActivity.java
+++ app/src/main/java/com/example/libraryland/BorrowBookActivity.java
@@ -1,24 +1,40 @@
 package com.example.libraryland;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class BorrowBookActivity extends AppCompatActivity {
+
+    private EditText userNameEditText, requestDateEditText;
+    private Button borrowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_borrow_book);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialiser les vues
+        userNameEditText = findViewById(R.id.userName);
+        requestDateEditText = findViewById(R.id.requestDate);
+        borrowButton = findViewById(R.id.borrowButton);
+
+        // Récupérer le nom d'utilisateur à partir de la session
+        SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+        String userName = sharedPreferences.getString("username", ""); // Récupérer le nom d'utilisateur
+        userNameEditText.setText(userName); // Remplir le champ User Name
+
+        // Récupérer la date actuelle et la formater
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String currentDate = dateFormat.format(new Date()); // Obtenir la date actuelle
+        requestDateEditText.setText(currentDate); // Remplir le champ Request Date
     }
 }
---
+++ app/src/main/java/com/example/libraryland/Hist_BorrowActivity.java
@@ -0,0 +1,14 @@
+package com.example.libraryland;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hist_BorrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hist_borrow);
+    }
+}
