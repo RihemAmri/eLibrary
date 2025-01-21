f21a218ca47e512dc553e45a1542871a98967b6c
RihemAmri <amririhem21@gmail.com>
2025-01-21 15:04:11 +0100

    users

--- app/src/main/java/com/example/libraryland/AdminActivity.java
+++ app/src/main/java/com/example/libraryland/AdminActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.cardview.widget.CardView;
 
 public class AdminActivity extends AppCompatActivity {
     @Override
@@ -15,12 +16,24 @@ public class AdminActivity extends AppCompatActivity {
         // Trouver la CardView pour les livres
         CardView booksCard = findViewById(R.id.books_card);
 
+
         // Ajouter un clic listener
         booksCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Lancer BooksListActivity
                 Intent intent = new Intent(AdminActivity.this, BooksListActivity.class);
+                startActivity(intent);
+            }
+        });
+        CardView usersCard = findViewById(R.id.users);
+
+        // Ajouter un clic listener pour les utilisateurs
+        usersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lancer UsersListActivity
+                Intent intent = new Intent(AdminActivity.this, UsersListActivity.class);
                 startActivity(intent);
             }
         });
