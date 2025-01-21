eb086084244d68a66bf7aeecca16e5d32b52530b
done <chalghoummaissa@gmail.com>
2025-01-21 02:13:05 +0100

    nav bar

--- app/src/main/java/com/example/libraryland/MainActivity.java
+++ app/src/main/java/com/example/libraryland/MainActivity.java
@@ -35,11 +35,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
         // Button to navigate to Historique
         FloatingActionButton historiqueButton = findViewById(R.id.historique);
         historiqueButton.setOnClickListener(v -> {
